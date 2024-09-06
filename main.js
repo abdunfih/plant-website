d5b2284a0015648838d53d564eca51a1823db6e2
Abdu Nafi <abdunafipp@gmail.com>
2024-09-06 09:01:17 +0530

    update

--- main.js
+++ main.js
@@ -16,13 +16,32 @@ navLink.forEach(link => {
 })
 
 // scrollHeade
+
 const scrollHeader = () => {
-    const header = document.getElementById("header");
-  
+    const header = document.getElementById("navbar");
+
     if (this.scrollY >= 50) {
-      header.classList.add("border-b", "border-yellow-300");
+        header.classList.add("border-b", "border-yellow-300");
     } else {
-      header.classList.remove("border-b", "border-yellow-300");
+        header.classList.remove("border-b", "border-yellow-300");
     }
-  };
-  
+};
+
+window.addEventListener('scroll', scrollHeader);
+
+// scrollUp
+
+const scrollUp = () => {
+    const scrollUpBtn = document.getElementById("scroll-up");
+
+    if (this.scrollY >= 250) {
+        scrollUpBtn.classList.remove("-bottom-1/2");
+        scrollUpBtn.classList.add("bottom-4");
+    } else {
+        scrollUpBtn.classList.add("-bottom-1/2");
+        scrollUpBtn.classList.remove("bottom-4");
+    }
+};
+
+window.addEventListener('scroll', scrollUp);
+
